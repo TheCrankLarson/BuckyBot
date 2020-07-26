542906f659f16504f3907bd874f9ed8759d1e081
TheCrankLarson <68806777+TheCrankLarson@users.noreply.github.com>
2020-07-26 10:33:53 +0100

    Removed superfluous files

--- functions.js
+++
@@ -1,8 +0,0 @@
-// FindRace function
-function FindRace(raceName)
-{
-    // We are looking for an event called (or containing) raceName, and will send results to informChannel
-
-    // 
-    return raceName + ' not found.';
-}
